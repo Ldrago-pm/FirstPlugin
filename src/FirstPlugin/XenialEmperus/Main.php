767784278286fc270d141249195ff216b9e9ea24
Ldrago-pm <101454316+Ldrago-pm@users.noreply.github.com>
2022-03-22 18:14:03 +0530

    Update Main.php

--- src/FirstPlugin/XenialEmperus/Main.php
+++ src/FirstPlugin/XenialEmperus/Main.php
@@ -21,8 +21,8 @@ use pocketmine\event\player\PlayerDropItemEvent;
 use jojoe77777\FormAPI\SimpleForm;
 use jojoe77777\FormAPI\CustomForm;
  
-class Main extends PluginBase 
-{
+class Main extends PluginBase implements Listener {
+
   
  public function onEnable(): void {
  $this->getLogger()->info("plugin test by rishi enabled successfully");
@@ -31,6 +31,35 @@ class Main extends PluginBase
  $this->getResource("Config.yml");
  $this->getServer()->getPluginManager()->registerEvents($this, $this);
     } 
+  
+ 
+ public function onJoin(PlayerJoinEvent $event) {
+     $sender = $event->getPlayer();
+     $item = ItemFactory::getInstance()->get(399, 0, 1);
+     $item->setCustomName("§r§l§aSKYBLOCK MENU\n§r§e(Right-Click)");
+     $sender->getInventory()->setItem(8, $item, true);
+  }
+ 
+ public function onClick(PlayerInteractEvent $event) {
+     $sender = $event->getPlayer();
+     $item = $event->getItem();
+     if ($item->getId() === 399 && $item->getCustomName() === "§r§l§aSKYBLOCK MENU\n§r§e(Right-Click)") {    
+        $this->sbmenu($sender);
+   }
+  }
+ 
+ 
+ public function onTransaction(InventoryTransactionEvent $event) {
+     $transaction = $event->getTransaction();
+     foreach ($transaction->getActions() as $action) {
+       $item = $action->getSourceItem();
+       $source = $transaction->getSource();
+       if ($source instanceof Player && $item->getId() === 399 && $item->getCustomName() === "§r§l§aSKYBLOCK MENU\n§r§e(Right-Click)") {
+          $event->cancel();
+     }
+   }
+ }
+  
   
  public function onCommand(CommandSender $sender, Command $command, string $label , array $args): bool {    
  
@@ -45,8 +74,8 @@ class Main extends PluginBase
    return true;
   }
   
-  public function newSimpleForm($player){
-      $form = new SimpleForm(function(Player $player, int $data = null){
+  public function sbmenu(player  $player) {
+      $$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(Function (Player $player, int $data = null){
           if($data === null){
            return true;
     }
@@ -83,5 +112,11 @@ $form->setContent("Your SkyBlock Menu!"
 $form->addButton("Your Profile\nClick To View", "0", "textures/ui/icon_steve");  
 $form->addButton("Fast Travel\nClick TO Open Menu", "0", "textures/ui/icon_bookshelf");
 $form->addButton("Bank\nClick To Open Bank", "0", "textures/ui/absorption_heart");
-            
-            
+$form->addButton("Jobs\nClick To Open Job Menu", "0", "textures/ui/icon_balloon");          
+$form->addButton("Shop\nClick To Open Shop", "0", "textures/ui/icon_cake");           
+$form->addButton("Island\nGo To Your Island", "0", "textures/blocks/grass_block_snow");
+$form->sendToPlayer($player)
+return $form;
+  }
+ 
+}        

5ba8cb83f12a1bb95256d000f373ae683093c16e
Ldrago-pm <101454316+Ldrago-pm@users.noreply.github.com>
2022-03-22 15:46:30 +0530

    Update Main.php

--- src/FirstPlugin/XenialEmperus/Main.php
+++ src/FirstPlugin/XenialEmperus/Main.php
@@ -1,1 +1,73 @@
 <?php 
+
+namespace FirstPlugin\XenialEmperus;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\events\Listener;
+use pocketmine\player\Player;
+use pocketmine\server;
+
+use pocketmine\command\Commamd;
+use pocketmine\command\CommandSender;
+
+use FirstPlugin\XenialEmperus\libs\jojoe77777\FormAPI\SimpleForm;
+class Main extends PluginBase 
+{
+  
+ public function onEnable(): void {
+ @mkdir($this->getDataFolder());
+ $this->saveDefaultConfig();
+ $this->getResource("Config.yml");
+   } 
+  
+ public function onCommand(CommandSender $sender, Command $command, string $label , array $args): bool {    
+ 
+  if($command->getName() == "Test"){
+  if($sender instanceof Player){
+      $this->newSimpleForm($sender);
+   } else { 
+  $sender->sendMessage("Run Command In-game Only");
+     }
+ }
+   
+   return true;
+  }
+  
+  public function newSimpleForm($player){
+      $form = new SimpleForm(function(Player $player, int $data = null){
+          if($data === null){
+           return true;
+    }
+        
+        switch($data){ 
+         case 0:
+                $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("profile"));  
+            break; 
+
+         case 1:
+             $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("fasttravel"))
+            break;
+            
+         case 2:
+             $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("bank"))
+             break;
+            
+          case 3:
+              $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("job"));     
+            break;
+            
+          case 4:       
+              $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("shop"));
+            break;
+            
+          case 5: 
+              $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("island"));        
+            break;
+     }       
+       
+});   
+$form->setTitle("SkyBlock Menu");
+$form->setContent("Your SkyBlock Menu!");
+  
+            
+            
